4a8dae80ef27530571b1ca73162578c68c1adbe4
ritesh <ritesh@thoughtwin.com>
2025-11-11 17:29:46 +0530

    change folder struchture

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: { 
     host: '0.0.0.0', 
     port: 5174,
+    allowedHosts: ['event-queue-app-production.up.railway.app'],
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
